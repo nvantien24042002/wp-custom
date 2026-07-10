59abe39f3a61ef7a1f303ee2c59271f441f6f068
Tien Nguyen <nguyenvantienthuanan12b3@gmail.com>
2026-07-10 10:20:17 +0700

    Day 3 : Loop Post Gird Basic

--- wp-content/themes/tientheme/functions.php
+++ wp-content/themes/tientheme/functions.php
@@ -1,6 +1,18 @@
 <?php
 function tientheme_setup(){
     add_theme_support("title-tag");
+    add_theme_support('post-thumbnails');
 }
 add_action("after_setup_theme",'tientheme_setup');
+
+function theme_enqueue_styles() {
+
+    wp_enqueue_style(
+        'main-style',
+        get_template_directory_uri() . '/assets/css/main.css'
+    );
+
+}
+
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 ?>
--- wp-content/themes/tientheme/index.php
+++ wp-content/themes/tientheme/index.php
@@ -1,7 +1,27 @@
 <?php
 get_header();
 ?>
-<h1>Trang chủ</h1>
+<div class="post-grid">
+    <?php if(have_posts()): ?>
+        <?php while(have_posts()) : the_post(); ?>
+            <div class="post-card">
+                <div class="post-thumb">
+                    <?php the_post_thumbnail(); ?>
+                </div>
+
+                <h2 class="post-title">
+                    <a href="<?php the_permalink(); ?>">
+                        <?php the_title(); ?>
+                    </a>
+                </h2>
+                <p class="post-excerpt">
+                    <?php the_excerpt(); ?>
+                </p>
+
+            </div>
+        <?php endwhile; ?>
+    <?php endif; ?>
+</div>
 <?php
 get_footer();
 ?>
